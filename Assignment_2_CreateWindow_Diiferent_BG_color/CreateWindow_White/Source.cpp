64df5536fff9a9ffe30e61f2aaa73c018ed748d8
hritujahedau <hrituja.hedau@gmail.com>
2020-07-05 20:01:56 -0700

    Different brush

---
+++ Assignment_2_CreateWindow_Diiferent_BG_color/CreateWindow_White/Source.cpp
@@ -0,0 +1,74 @@
+#include<Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hInstancePrev, LPSTR lszptrCommandLine, int iCmdShow) {
+
+	// declaration
+	WNDCLASSEX wndClassEx;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyWindow");
+
+	//code
+	// initilization
+
+	wndClassEx.hInstance = hInstance;
+	wndClassEx.lpszClassName = szAppName;
+	wndClassEx.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndClassEx.cbSize = sizeof(wndClassEx);
+	wndClassEx.cbClsExtra = 0;
+	wndClassEx.cbWndExtra = 0;
+	wndClassEx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClassEx.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndClassEx.lpszMenuName = NULL;
+	wndClassEx.style = CS_HREDRAW | CS_VREDRAW;
+	wndClassEx.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClassEx.lpfnWndProc = WndProc;
+
+	RegisterClassEx(&wndClassEx);
+
+	hwnd = CreateWindow(
+		szAppName,
+		TEXT("First Assignment"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//message loop
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+
+
+	return ((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	switch (uMsg)
+	{
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		break;
+	}
+
+	return (DefWindowProc(hwnd, uMsg, wParam, lParam));
+
+}
